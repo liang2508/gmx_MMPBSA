1569438fa209fbdcb195b1d677a95339e6b69ec2
liang2508 <83101939+liang2508@users.noreply.github.com>
2021-10-05 10:50:03 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+[![pypi](https://img.shields.io/pypi/v/gmx-MMPBSA)](https://pypi.org/project/gmx-MMPBSA/)
+[![support](https://img.shields.io/badge/support-JetBrains-brightgreen)](https://www.jetbrains.com/?from=gmx_MMPBSA)
+[![python](https://img.shields.io/badge/python-v3.x-blue)]()
+[![Downloads](https://pepy.tech/badge/gmx-mmpbsa)](https://pepy.tech/project/gmx-mmpbsa)
+[![DOI](https://img.shields.io/badge/DOI-10.1021%2Facs.jctc.1c00645-blue)](https://pubs.acs.org/doi/10.1021/acs.jctc.1c00645)
+
+# Welcome to gmx_MMPBSA!
+gmx_MMPBSA is a new tool based on AMBER's MMPBSA.py aiming to perform end-state free energy calculations with GROMACS 
+files. It works with all GROMACS versions along with AmberTools20 or 21.
+
+**Please see the documentation [here](https://valdes-tresanco-ms.github.io/gmx_MMPBSA/getting-started/)**
+
+## Cite us
+
+<a href="https://www.scimagojr.com/journalsearch.php?q=5100155074&amp;tip=sid&amp;exact=no" title="SCImago Journal 
+&amp; Country Rank"><img border="0" align="right" src="https://www.scimagojr.com/journal_img.php?id=5100155074" 
+alt="SCImago Journal &amp; Country Rank"  /></a>
+
+Valdés-Tresanco, M.S., Valdés-Tresanco, M.E., Valiente, P.A. and Moreno E. **gmx_MMPBSA: A New Tool to Perform 
+End-State Free Energy Calculations with GROMACS**. _Journal of Chemical Theory and Computation_, 2021. 
+https://pubs.acs.org/doi/10.1021/acs.jctc.1c00645
+
+Please also consider citing MMPBSA.py's paper:
+
+Bill R. Miller, T. Dwight McGee, Jason M. Swails, Nadine Homeyer, Holger Gohlke, and Adrian E. Roitberg. **MMPBSA.py: 
+An Efficient Program for End-State Free Energy Calculations**. _Journal of Chemical Theory and Computation_, 2012 8 
+(9), 3314-3321. https://pubs.acs.org/doi/10.1021/ct300418h
+
+---------------------------------------
+
+Authors:
+- Mario Sergio Valdés Tresanco, PhD Student. _University of Medellin, Colombia_
+- Mario Ernesto Valdés Tresanco, PhD Student. _University of Calgary, Canada._
+- Pedro Alberto Valiente, PhD. _University of Toronto, Canada_
+- Ernesto Moreno Frías, PhD. _University of Medellin, Colombia_
+
+---------------------------------------
---
+++ setup.py
@@ -0,0 +1,52 @@
+# ##############################################################################
+#                           GPLv3 LICENSE INFO                                 #
+#                                                                              #
+#  Copyright (C) 2020  Mario S. Valdes-Tresanco and Mario E. Valdes-Tresanco   #
+#  Copyright (C) 2014  Jason Swails, Bill Miller III, and Dwight McGee         #
+#                                                                              #
+#   Project: https://github.com/Valdes-Tresanco-MS/gmx_MMPBSA                  #
+#                                                                              #
+#   This program is free software; you can redistribute it and/or modify it    #
+#  under the terms of the GNU General Public License version 3 as published    #
+#  by the Free Software Foundation.                                            #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful, but         #
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  #
+#  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License    #
+#  for more details.                                                           #
+# ##############################################################################
+
+from setuptools import setup, find_packages
+import versioneer
+import sys
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+#if sys.version_info[:2] < (3, 8):
+#    raise RuntimeError("seaborn requires python >= 3.8.")
+
+setup(
+    name='gmx_MMPBSA',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    package_data={"GMXMMPBSA": ["data/*", 'data/gmxMMPBSA/*', 'GMXMMPBSA.sh']},
+    license='GPLv3',
+    author='Mario S. Valdes-Tresanco and Mario E. Valdes-Tresanco ',
+    author_email='mariosergiovaldes145@gmail.com',
+    maintainer='Mario S. Valdes-Tresanco',
+    maintainer_email='mariosergiovaldes145@gmail.com',
+    url='https://github.com/Valdes-Tresanco-MS/gmx_MMPBSA',
+    description="gmx_MMPBSA is a new tool based on AMBER's MMPBSA.py aiming to perform end-state free energy  "
+                "calculations with GROMACS files",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    keywords=['gmx_MMPBSA', 'MMPBSA', 'MMGBSA', 'GROMACS', 'AmberTools'],
+    install_requires=['pandas>=1.2.2', 'seaborn>=0.11.1', 'mpi4py>=3.0.3', 'scipy>=1.6.1'],
+    entry_points={
+        "console_scripts": [
+            "gmx_MMPBSA=GMXMMPBSA.app:gmxmmpbsa",
+            "gmx_MMPBSA_ana=GMXMMPBSA.app:gmxmmpbsa_ana",
+            "gmx_MMPBSA_test=GMXMMPBSA.app:gmxmmpbsa_test"]}
+)
